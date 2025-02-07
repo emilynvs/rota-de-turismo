fc5fc381c83cf7a48bc60a6fb3efdd6c0e956082
Emily Neves <emilynascimento22@outlook.com>
2025-02-07 11:00:17 -0300

    Update pontosturisticos.py

--- pontosturisticos.py
+++ pontosturisticos.py
@@ -1,8 +1,9 @@
 import heapq
 import sys
+import networkx as nx
+import matplotlib.pyplot as plt
 # heappush: Adiciona um elemento à fila com sua prioridade associada.
 # heappop: Remove e retorna o elemento com a prioridade mais alta (o elemento com o menor valor).
-
 def dijkstra(pontos, inicio, fim):
     distancias = {no: sys.maxsize for no in pontos}
     distancias[inicio] = 0
@@ -10,17 +11,16 @@ def dijkstra(pontos, inicio, fim):
     rota = []
     visitados = set() #evita a repetição de nós visitados
     prioridade = [(0, inicio)]
+    
     while prioridade:
-
         distancia_atual, no_atual = heapq.heappop(prioridade)
         if no_atual in visitados:
             continue
-
-
         visitados.add(no_atual)
-        
+
         if no_atual == fim:
             break
+
         for vizinho, peso in pontos[no_atual]:
             nova_distancia = peso + distancia_atual
 
@@ -28,14 +28,13 @@ def dijkstra(pontos, inicio, fim):
                 origem[vizinho] = no_atual
                 distancias[vizinho] = nova_distancia
                 heapq.heappush(prioridade,(nova_distancia, vizinho))
-
     no_atual = fim
     while no_atual  is not None:
         rota.append(no_atual)
         no_atual = origem[no_atual]
-       
-
+    print(f"A distância total da rota é: {distancias[fim]} unidades.")
     return rota[::-1]
+
 def verificacao(resultado):
     match resultado:
         case '1':
@@ -48,41 +47,62 @@ def verificacao(resultado):
             return'jardim botânico'
         case '5':
             return'parque sólon de lucena'
+        case '6':
+            return 'centro cultura de São Francisco'
         case _:
-            return False
-        
+            return False       
 
 pontos = {
-    'praia de tambaú': [('jardim botânico', 6.4), ('piscinas naturais de picãozinho', 0.11), ('mercado de artesanato paraibano', 0.7)],
+    'praia de tambaú': [('jardim botânico', 6.4), ('piscinas naturais de picãozinho', 0.11), ('mercado de artesanato paraibano', 0.7), ('centro cultura de São Francisco', 7)],
     'mercado de artesanato paraibano': [('jardim botânico', 7.9), ('praia de tambaú', 0.7)],
-    'piscinas naturais de picãozinho': [('praia de tambaú', 0.11)],
-    'jardim botânico': [('mercado de artesanato paraibano', 7.9), ('praia de tambaú', 6.4), ('parque sólon de lucena', 3.2)],
-    'parque sólon de lucena': [ ('jardim botânico',3.2)],   
-    # 'centro cultura de São Francisco': [('parque sólon de lucena', 2), ('jardim botânico', 4.9), ('praia de tambaú', 7.9), ('piscinas naturais de picãozinho', 7.8)],
+    'piscinas naturais de picãozinho': [('praia de tambaú', 0.11), ('centro cultura de São Francisco', 7.8)],
+    'jardim botânico': [('mercado de artesanato paraibano', 7.9), ('praia de tambaú', 6.4), ('parque sólon de lucena', 3.2), ('centro cultura de São Francisco', 4.9)],
+    'parque sólon de lucena': [ ('jardim botânico',3.2), ('centro cultura de São Francisco', 2)],   
+    'centro cultura de São Francisco': [('parque sólon de lucena', 2), ('jardim botânico', 4.9), ('praia de tambaú', 7.9), ('piscinas naturais de picãozinho', 7.8)],
 }
 
-while True:
+def desenhar_grafo(pontos, rota):
+    G = nx.Graph()
+    for no in pontos:
+        G.add_node(no)
+        for arestas in pontos[no]:
+            G.add_edge(no,arestas[0])
+    lista_de_arestas = [(rota[i], rota[i+1]) for i in range(len(rota)-1)]
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True)
+    nx.draw_networkx_nodes(G, pos, nodelist= rota, node_color='lightcoral')
+    nx.draw_networkx_edges(G, pos, edgelist = lista_de_arestas, edge_color = 'lightcoral', width = 5)
+    plt.show()
+
+def msg ():
     print("Digite o número de acordo com as opções:")
     print("1 - Praia de Tambaú \
 \n2 - Mercado de Artesanato Paraibano \
 \n3 - Piscinas Naturais de Picãozinho \
 \n4 - Jardim Botânico \
-\n5 - Parque Sólon de Lucena")
+\n5 - Parque Sólon de Lucena \
+\n6 - Centro Cultura de São Francisco")
+
+inicio = False
+fim = False
+while not inicio:
+    msg()
     inicio = input("Digite onde iniciará o passeio: ")
-    inicio = verificacao(inicio)
-    print(f"Você escolheu: {inicio}")
-    if not inicio:
-        print("Resposta inválida. Tente novamente")
-        continue
+    inicio = verificacao(inicio)  
+    if not inicio:  
+        print("Resposta inválida.")
+print(f"Você escolheu: {inicio}")
 
-    fim = input("Digite  onde será o último ponto: ")
+while not fim: 
+    msg()
+    fim = input("Digite onde será o último ponto: ")
     fim = verificacao(fim)
-    if not fim: 
-        print("Resposta inválida. Tente novamente")
-        continue
-    origem = dijkstra(pontos, inicio, fim)
-    print(f"Sua rota que começa em {inicio} e termina em {fim} segue por: ")
+    if not fim:
+        print("Resposta inválida.")
+print(f"Você escolheu: {fim}")
 
-    for n, local in enumerate(origem):
-        print(f'{n} => {local}')
-    break
+origem = dijkstra(pontos, inicio, fim)
+print(f"Sua rota que começa em {inicio} e termina em {fim} segue por: ")
+for n, local in enumerate(origem):
+    print(f'{n} => {local}')
+desenhar_grafo(pontos, origem)
